6e1c50438548fcd59ca95deb11e1807a906a9b7e
77Vincent <wentianqi77@outlook.com>
2017-01-08 21:44:40 +0800

    image href

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ $('#add').on('click', () => {
         counter++
         $slider
             .children('.slide:first-child')
-            .before('<div class="slide" style="background-image: url(/src/assets/bg-' + parseInt(counter + 4) + '.jpg)"></div>')
+            .before('<div class="slide" style="background-image: url(http://7xr7xo.com1.z0.glb.clouddn.com/bg-' + parseInt(counter + 4) + '.jpg)"></div>')
     }
 })
