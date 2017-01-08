3cdcb89922b230dbf46736be1a54edd074c18510
77Vincent <wentianqi77@outlook.com>
2017-01-08 22:16:39 +0800

    compiled vinslider and readme updates

--- README.md
+++ README.md
@@ -1,28 +1,24 @@
 # Vinslider 
 
-Light weight, responsive, native Javascript slider module. Highly customizable.
-
-## Demo
+Light weight, native Javascript slider module. 
 
 http://www.77webtech.com/vinslider
 
-## Installation
+## Setup
 
 Install through npm
 
     npm install --save vinslider
     
-Get HTML ready
+Markup
 
     <div class="vinslider">
-        <ul>
-            <li>content</li>
-            <li>content</li>
-            ...
-        </ul>
+        <div>content</div>
+        <div>content</div>
+        ...
     <div>
     
-Instantiate in Javascript
+Javascript
     
     import Vinslider from 'vinslider'
     
@@ -33,160 +29,41 @@ Instantiate in Javascript
 
 ## Configurations
 
-#### mode : String
-
-    : 'fade' 
-
-    : 'slide' (Default)
-
-Under carousel mode, pager will be hidden by default.
-
 #### speed : Number
 
-    : 750 (Default, millisecond)
+    : 750 (millisecond)
 
-The transition duration of sliding.
+The duration of transition
 
 #### duration : Number
 
-    : 5000 (Default, millisecond)
+    : 5000 (millisecond)
     
-How much time each slide stays.
+How much time each slide stays
 
 #### amount: Number
  
-    : 1 (Default)
+    : 1 
 
-If the value is greater than or equal to the total amount of items, the controller will be hidden.
-
-####  gutter : Number
-
-    : 0 (Default, pixel)
-    
-Add a gutter in between each items.
-
-#### startFrom: Number
-
-    : 0 (Default)
-
-From which item the slider starts, the index of the first slide is 0 and so on.
-
-#### moveBy : Number
-
-    : 1 (Default)
-    
-How many items will pass by each slide.
+How many slides being shown at a time
 
 #### isAutoplay : Boolean
 
     : true (Default)
 
-    : false
-    
-If auto-play the slider
+Vinslider will autoplay
 
 #### isInfinite : Boolean
 
     : true (Default)
 
-    : false
-
-If the slider will go back to the first item after reaching the last one.
-
-#### isScrollable : Boolean
-
-    : true
-
-    : false (Default)
-
-Mouse wheel scrolling could control the slider.
-
-#### isVertical : Boolean
-
-    : true
-
-    : false (Default)
-    
-If slider is displayed vertically
-
-#### isPercentGutter : Boolean
-
-    : true
-
-    : false (Default)
-
-The unit of gutter will be percentage based on the width of each slide. Only works when the gutter is also declared, if not, no effect or error will be met.
-
-#### isForward : Boolean
-
-    : true (Default)
-
-    : false
-
-Slider goes from left to right, or top to bottom. This only affects the auto playing. User events will still respect the direction according to their navigation.
-
-#### isUseItemSize : Boolean
-
-    : true
-
-    : false (Default)
-
-Force vinslider to have the height( width for the vertical mode ) which comes from its biggest slide item.
-
-#### isFillWrapper : Boolean
-
-    : true
-
-    : false (Default)
-    
-Each slide in Vinslider will have a inline style of height of 100% to fit its wrapper.
-
-## Callback Functions
-
-Useage: After a Vinslider instance is instantiated, these functions could be called to manipulated the instance anytime when there is a need for callbacks.
-
-Example: vinslider.goto(3);
-> Here means navigating this vinslider instance to goto the 4th slide when this piece of code is called.
-
-#### goto(index); 
-
-    instance.goto(0)
-
-Navigate a vinslider instance to goto its first slide.
-
-#### resize();
-
-    instance.resize()
-    
-Vinslider already has the ability to resize its self under a window.onresize event, but when the size of its wrapper changes without a window resize event occurring, use this function to resize the size of vinslider to fit its new wrapper. 
-
-#### ifAutoplay();
-
-    instance.ifAutoplay(false)
-    
-Stop or resume a vinslider instance.
-
-#### reAmount(value);
-
-    instance.reAmount(3)
-    
-Reset the amount of items to be displayed based upon the new given value at a time when under carousel mode, mostly for responsive. 
-
-#### rebuild();
-
-    instance.rebuild({
-    	// new config goes here
-    })
-    
-Totally rebuild a Vinslider with new configurations from an existing instance. Old specified configurations will not be removed, adding new configurations wil override the old ones or the default ones otherwise the old configs will still work.
+Vinslider doesn't stop when reaching the last slide
 
 ## Browser Compatibility
 
-IE 8: Not supported
-
 IE 9: No CSS transition effect
 
-IE 10+: Fully supported
+IE 10: Fully supported
 
 ## License
 
---
+++ vinslider/vinslider.compiled.js
@@ -0,0 +1,152 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * @Title:     Vinslider
+ * @Descr:     A lightweight slider/carousel module in native Javascript
+ * @Package:   Javascript
+ * @Copyright: Released under the MIT license: http://opensource.org/licenses/MIT
+ * @Author:    Vincent Wen <www.77webtech.com/about> <wentianqi77@outlook.com>
+ */
+
+var Vinslider = function () {
+    function Vinslider(object) {
+        var _this = this;
+
+        var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+            _ref$amount = _ref.amount,
+            amount = _ref$amount === undefined ? 1 : _ref$amount,
+            _ref$speed = _ref.speed,
+            speed = _ref$speed === undefined ? 750 : _ref$speed,
+            _ref$duration = _ref.duration,
+            duration = _ref$duration === undefined ? 3000 : _ref$duration,
+            _ref$isAutoplay = _ref.isAutoplay,
+            isAutoplay = _ref$isAutoplay === undefined ? true : _ref$isAutoplay,
+            _ref$isInfinite = _ref.isInfinite,
+            isInfinite = _ref$isInfinite === undefined ? true : _ref$isInfinite,
+            _ref$prev = _ref.prev,
+            prev = _ref$prev === undefined ? '' : _ref$prev,
+            _ref$next = _ref.next,
+            next = _ref$next === undefined ? '' : _ref$next;
+
+        _classCallCheck(this, Vinslider);
+
+        this.init(object)(amount)(speed)(isInfinite)(duration)(isAutoplay)(prev, next);
+
+        var observer = new MutationObserver(function () {
+            _this.init(object)(amount)(speed)(isInfinite)(duration)(isAutoplay)(prev, next);
+        });
+        observer.observe(object, {
+            attributes: true,
+            childList: true,
+            characterData: true
+        });
+    }
+
+    _createClass(Vinslider, [{
+        key: 'init',
+        value: function init(object) {
+            var _this2 = this;
+
+            this.items = Array.prototype.slice.call(object.children);
+
+            // Layout 
+            return function (amount) {
+                var width = 100 / amount;
+
+                _this2.items.forEach(function (v, i) {
+                    v.style.width = width + '%';
+                    v.style.position = 'absolute';
+                    v.style.left = width * i + '%';
+                });
+
+                object.style.position = 'relative';
+                object.style.overflow = 'hidden';
+                object.style.visibility = 'visible';
+
+                // Set transition 
+                return function (speed) {
+                    var time = speed / 1000 + 's';
+
+                    _this2.items.forEach(function (v, i) {
+                        v.style.WebkitTransition = 'left ' + time + ' ease';
+                        v.style.MozTransition = 'left ' + time + ' ease';
+                        v.style.OTransition = 'left ' + time + ' ease';
+                        v.style.transition = 'left ' + time + ' ease';
+                    });
+
+                    // Set movement and direction
+                    return function (isInfinite) {
+                        _this2.index = 0;
+
+                        var loop = function loop() {
+                            _this2.items.forEach(function (v, i) {
+                                v.style.left = width * (i - _this2.index) + '%';
+                                v.className = v.className.replace(' vinactive', '');
+                            });
+                            _this2.items[_this2.index].className += ' vinactive';
+                        };
+
+                        var forwards = function forwards() {
+                            ++_this2.index;
+                            while (_this2.index === _this2.items.length - amount + 1) {
+                                if (isInfinite) {
+                                    _this2.index = 0;
+                                } else {
+                                    --_this2.index;
+                                }
+                            }
+                            loop();
+                        };
+
+                        var backwards = function backwards() {
+                            --_this2.index;
+                            while (_this2.index < 0) {
+                                if (isInfinite) {
+                                    _this2.index = _this2.items.length - amount;
+                                } else {
+                                    ++_this2.index;
+                                }
+                            }
+                            loop();
+                        };
+
+                        // Start autoplay
+                        return function (duration) {
+                            window.clearTimeout(_this2.timer);
+                            _this2.timer = window.setInterval(function () {
+                                forwards();
+                            }, duration);
+
+                            // Stop autoplay
+                            return function (isAutoplay) {
+                                if (!isAutoplay) window.clearTimeout(_this2.timer);
+
+                                // Controllers
+                                return function (prev, next) {
+                                    document.querySelector(prev).onclick = function () {
+                                        backwards();
+                                    };
+                                    document.querySelector(next).onclick = function () {
+                                        forwards();
+                                    };
+                                };
+                            };
+                        };
+                    };
+                };
+            };
+        }
+    }]);
+
+    return Vinslider;
+}();
+
+exports.default = Vinslider = Vinslider;
