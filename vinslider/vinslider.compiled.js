8827fdfe067faea9fd7b4cc8c94d9803f42c5881
77Vincent <wentianqi77@outlook.com>
2017-01-09 00:04:47 +0800

    build

--- vinslider/vinslider.compiled.js
+++ vinslider/vinslider.compiled.js
@@ -1,8 +1,6 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -149,4 +147,12 @@ var Vinslider = function () {
     return Vinslider;
 }();
 
-exports.default = Vinslider = Vinslider;
+if (typeof define === "function" && define.amd) {
+    define('Vinslider', [], function () {
+        return Vinslider;
+    });
+} else if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === "object" && exports) {
+    module.exports = Vinslider;
+} else {
+    window.Vinslider = Vinslider;
+}
